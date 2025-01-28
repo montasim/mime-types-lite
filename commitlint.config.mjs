270bdabe0cc5b742d18857cd8096e7ccbfbc843a
Ｍ♢ＮＴΛＳＩＭ <montasimmamun@gmail.com>
2025-01-28 20:47:13 +0600

    fix: generate a new version and push tags

--- commitlint.config.mjs
+++ commitlint.config.mjs
@@ -1,26 +1,3 @@
 export default {
     extends: ['@commitlint/config-conventional'],
-    rules: {
-        'subject-empty': [2, 'never'],
-        'type-empty': [2, 'never'],
-        'body-leading-blank': [1, 'always'],
-        // Allow "chore(release): x.x.x" commits
-        'type-enum': [
-            2,
-            'always',
-            [
-                'feat',
-                'fix',
-                'docs',
-                'style',
-                'refactor',
-                'perf',
-                'test',
-                'build',
-                'ci',
-                'chore',
-            ],
-        ],
-        'scope-enum': [2, 'always', ['release']],
-    },
 };
